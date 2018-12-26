bd4899d706399edf2a7dfc80007964594b64ef74
Konstantin Kostenski <konstantin_kostenski@yahoo.com>
2018-12-26 20:22:29 +0200

    Comments commit.

--- Forum/Controllers/CommentController.cs
+++ Forum/Controllers/CommentController.cs
@@ -1,16 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Forum.Data;
+using Forum.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly ForumDbContext context;
+
+        public CommentController(ForumDbContext context)
+        {
+            this.context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("/Topic/Details/{id}/Comment/Create")]
+        public IActionResult Create(int id)
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("/Topic/Details/{TopicId}/Comment/Create")]
+        public IActionResult Create(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.CreatedDate = DateTime.Now;
+                comment.LastUpdatedDate = DateTime.Now;
+
+                string authorId = context.Users
+                    .Where(u => u.UserName == User.Identity.Name)
+                    .SingleOrDefault()
+                    .Id;
+
+                comment.AuthorId = authorId;
+
+                Topic topic = context.Topics
+                    .FirstOrDefault(t => t.Id == comment.TopicId);
+
+                topic.LastUpdatedDate = DateTime.Now;
+
+                context.Comments.Add(comment);
+
+                context.SaveChanges();
+
+                return Redirect($"/Topic/Details/{comment.TopicId}");
+            }
+
+            return View(comment);
         }
     }
 }
